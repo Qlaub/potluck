d23cd98031f3b1a65182ee15b5e6fbf907ebd173
Alex Glaubitz <e.pirazzi@gmail.com>
2022-05-23 22:33:59 -0400

    stripe checkout functioning, auto seeding of dishes

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -1,24 +1,51 @@
 const button = document.querySelector('#checkoutBtn');
 
-button.addEventListener('click', () => {
-  console.log('checkout');
-  fetch('/create-checkout-session', {
-    method: 'POST',
+async function getDishes(ids, quantities) {  
+
+  const response = await fetch('/api/restaurants/dishes/', {
+    method: 'PUT',
     headers: {
       'Content-Type': 'application/json'
     },
+    body: JSON.stringify({ids: ids})
+  });
+
+  let dishes;
+
+  response.ok ? dishes = await response.json() : alert(response.statusText);
+
+  for (let i=0; i < dishes.length; i++) {
+    dishes[i].quantity = quantities[i];
+  }
+
+  console.log(dishes);
+
+  return dishes;
+};
+
+async function checkout(dishes) {
+  fetch('/api/checkout', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
     body: JSON.stringify({
-      items: [
-        { id: 1, quantity: 3 },
-        { id: 2, quantity: 1 }
-      ]
+      items: dishes
     })
   }).then(res => {
     if (res.ok) return res.json()
     return res.json().then(json => Promise.reject(json))
   }).then(({ url }) => {
-    window.location = url
+    window.location = url;
   }).catch(err => {
-    console.log(err.error)
+    console.log(err)
   });
+}
+
+button.addEventListener('click', async () => {
+  const dishes = await getDishes(testIds, quantities);
+  const pay = await checkout(dishes);
 });
+
+const testIds = [1,2,3];
+const quantities = [2, 2, 2];
