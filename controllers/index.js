e9a843c05cb0b45ba074edf8c58d62766027d910
Malik <malikspruill@gmail.com>
2022-05-24 01:08:51 -0400

    Added to mainscreen, restaurant, and orderSearch routes in controllers sub-directory to prepare for handlebards redirects when a customer progresses through different pages for the app.

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const mainscreenRoutes = require('./mainscreen-routes');
+const restaurantRoutes = require('./restaurant-routes');
+const ordersearchRoutes = require('./ordersearch-routes');
 
 router.use('/api', apiRoutes);
-
+router.use('/', mainscreenRoutes);
+router.use('/restaurant', restaurantRoutes);
+router.use('/orderSearch', ordersearchRoutes);
 module.exports = router;
---
+++ controllers/mainscreen-routes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+
+//checks if a user is already logged in
+router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) { 
+      res.redirect('/');
+      return;
+    }
+    res.render('signup');
+});
+
+//checks if a user is already logged in
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) { 
+      res.redirect('/');
+      return;
+    }
+    res.render('login');
+});
+
+module.exports = router;
